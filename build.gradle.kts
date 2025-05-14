bff8e7223a333ed7fe24d10585113a7047fa5fd4
Nikolay Groznykh <nikolay@groznykh.ru>
2025-05-14 15:18:46 +0300

    Update kotiln version and fixed build image to bellsoft

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.spring") version "2.1.20"
+    kotlin("jvm") version "2.2.0-Beta2"
+    kotlin("plugin.spring") version "2.2.0-Beta2"
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -10,8 +12,10 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(23)
+        languageVersion = JavaLanguageVersion.of(24)
     }
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
 }
 
 repositories {
@@ -19,27 +23,30 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-data-rest")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
+    jvmToolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
     compilerOptions {
+        jvmTarget = JvmTarget.JVM_24
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
-    jvmToolchain(23)
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks {
+    bootBuildImage {
+        environment.set(mapOf("BP_JVM_VERSION" to "24.0.1"))
+    }
+    test {
+        useJUnitPlatform()
+    }
 }
